3ccdd1cf8265c405c58ff664cf9705217fe032d9
Tina Tram <tinatram07@gmail.com>
2024-04-29 20:21:06 -0500

    save changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
 import React, {useState} from "react";
+import ReactDOM from 'react-dom'
 import styled from "styled-components";
 
+function App () {
+    return(
+    <p>Hello</p>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-import App from "./App";
+import App from "./components/App.jsx";
 
 ReactDOM.render(
     <App />,
